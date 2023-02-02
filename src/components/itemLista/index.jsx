0288c87c21420516efa532aef68505c7994195fc
Jonas <jonasafribeiro@gmail.com>
2023-02-02 16:14:51 -0300

    Fix: Add a place to the removeHandler

--- src/components/itemLista/index.jsx
+++ src/components/itemLista/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import TrashCan from '../../assets/img/trash.svg'
+import { removeHandler } from '../../script/removeHandler'
 
 import './index.css'
 
@@ -16,11 +17,4 @@ export default function ListItem(prop) {
             </div>
         </li>
     )
-}
-
-function removeHandler(prop){
-    prop.callback( current => current.filter( element => (element.name !== prop.name || element.type !== prop.type || element.value !== prop.value)  ) );
-    let data = JSON.parse(localStorage.getItem("@NuKenzie"));
-    localStorage.setItem("@NuKenzie", JSON.stringify(data.filter(element => (element.name !== prop.name || element.type !== prop.type || element.value !== prop.value))))
-
 }
